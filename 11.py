8176e94bfac1e449ab833c88f79811bf73bbc1b5
Komkov2000Evgeny <116486580+Komkov2000Evgeny@users.noreply.github.com>
2023-03-12 12:52:35 +0300

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,20 @@
+#1
+def fractional_number_generator(n):
+    from random import uniform
+    for _ in range(n):
+        yield uniform(0,n)
+        
+print(list(fractional_number_generator(3)))
+
+#2
+def iterchain(fst_list, snd_list):
+    from itertools import chain
+    return list(chain(fst_list, snd_list))
+
+print(iterchain([1, 2], [3, 4]))
+
+#3
+def responses_creator(item_ids):
+    return [dict(item_id = item_id) for item_id in item_ids]
+    
+print(responses_creator([0,1,2,3]))
